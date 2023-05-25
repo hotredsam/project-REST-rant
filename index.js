dad1eb419dc3fc7325961e1bde58a66a908a6603
hotredsam <hotredsam@gmail.com>
2023-05-24 21:11:34 -0700

    completed steps 2-4

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+    res.send('Hello world!')
+})
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-});
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
